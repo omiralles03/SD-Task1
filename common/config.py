0486649d15ff669c9bed544dd25773e4a2a82a38
Oupman Miralles <oupman.miralles@qmr.cat>
2026-03-30 14:50:50 +0200

    mod PORTS for docker

--- common/config.py
+++ common/config.py
@@ -3,5 +3,5 @@ import os
 REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 RABBIT_HOST = os.getenv("RABBIT_HOST", "localhost")
 
-REDIS_PORT = 6379
-RABBIT_PORT = 5672
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+RABBIT_PORT = int(os.getenv("RABBIT_PORT", 5672))
